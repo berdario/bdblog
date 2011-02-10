20124689bee0bfe5b6d43e6d5833e873e6de11ab
Dario Bertini <berdario@gmail.com>
2011-02-10 12:39:31 +0100

    Dropped __init__ from models, now using the Model default field

--- blog/models.py
+++ blog/models.py
@@ -8,27 +8,14 @@ now = datetime.datetime.now
 class Post(Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	pub_date = models.DateTimeField('last change')
-	orig_date = models.DateTimeField('date published')
-	
-	def __init__(self, title="", text="", orig_date=None, pub_date=None, *args, **kwargs ):
-		if not pub_date:
-			pub_date = now()
-		if not orig_date:
-			orig_date = pub_date
-		Model.__init__(self, title=title, text=text, orig_date=orig_date, pub_date=pub_date, *args, **kwargs )
+	pub_date = models.DateTimeField('last change', default=now)
+	orig_date = models.DateTimeField('date published', default=now)
 	
 	def __unicode__(self):
 		return self.title
 
 class PostDiff(Post):
 	original = models.OneToOneField(Post, null=True)
-	
-	def __init__(self, original, text, *args, **kwargs ):
-		title, pub_date, orig_date = original.title, original.pub_date, original.orig_date
-		if pub_date == orig_date:
-			original = None
-		Post.__init__(self, title, text, orig_date=orig_date, pub_date=pub_date, original=original, *args, **kwargs )
 	
 	def __unicode__(self):
 		return self.title + " - " + str(self.pub_date.ctime())
