d3846ad9cae29626c14b3ce84270f0872d149705
Dario Bertini <berdario@gmail.com>
2012-01-28 22:53:29 +0100

    Factored out the choice between html and json in a decorator

--- bdblog/views.py
+++ bdblog/views.py
@@ -11,41 +11,43 @@ from models import from_tags, get_post, get_posts, PostForm
 months = {"jan":1, "feb":2, "mar":3, "apr":4, "may": "5", "jun":6,
 	"jul": 7, "aug": 8, "sep": 9, "oct": 10, "nov": 11, "dec": 12,
 	"january":1, "february":2, "march":3, "april":4, "june":6,
-	"july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12}
-	
-def index(request, page, admin=False):
-	if 'application/json' in request.META['HTTP_ACCEPT_ENCODING']:
-		return HttpResponse("""TODO""", mimetype='application/json')
-	else:
-		return render_to_response('main', locals())
+	"july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12} 
 
+def json_or_template(template):
+	def outer(view):
+		def wrapped(*args, **kwargs):
+			result, context = view(*args, **kwargs)
+			if _accept_json(args[0]):
+				return HttpResponse(json.serialize(result), mimetype='application/json')
+			else:
+				return render_to_response(template, context)
+		return wrapped
+	return outer
+
+@json_or_template('main')
+def index(request, page, admin=False):
+	return ["TODO"], locals() 
+
+@json_or_template('single_post')
 def post(request, slug, year, month, day, admin=False):
 	d = None
 	if year and month and day:
 		month = _handle_verbose_month(month)
 		d = date(int(year), month, int(day))
 	p = get_post(slug, d)
-	if _accept_json(request):
-		return HttpResponse(json.serialize([p]), mimetype='application/json')
-	else:
-		return render_to_response('single_post', locals())
+	return [p], locals()
 
+@json_or_template('posts')
 def posts(request, year, month, day, page, admin=False):
 	month = _handle_verbose_month(month)
 	post_list = get_posts(year, month, day, page if page else 1)
-		
-	if _accept_json(request):
-		return HttpResponse(json.serialize(post_list), mimetype='application/json')
-	else:
-		return render_to_response('posts', locals())
-		
+	return post_list, locals()
+
+@json_or_template('posts')
 def tags(request, tags, page, separator="\.", admin=False):
 	tag_list = [sub(separator," ",tag) for tag in tags.split("+")]
 	post_list = from_tags(tag_list, page if page else 1)
-	if _accept_json(request):
-		return HttpResponse(json.serialize(post_list), mimetype='application/json')
-	else:
-		return render_to_response('posts', locals())
+	return post_list, locals()
 
 def publish_post(request, slug):
 	return HttpResponse("TODO")
